1bc64940b56e538ffcd2004acff2a330690be302
Claire Scheele <cscheele@berkeley.edu>
2018-07-07 10:44:41 -0700

    Find documents with a query filter.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var findDocuments = function(db, callback) {
   // Get the documents collection
   var collection = db.collection('documents');
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
+  collection.find({'a': 3}).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs);
